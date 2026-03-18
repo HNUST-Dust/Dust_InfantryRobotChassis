88b6228770315891ab2598e8dc425e9dfc0fabd0
NoneOfEver <noneofever@gmail.com>
2026-03-18 19:34:38 +0800

    fix:整改UI

--- Device/dvc_referee.cpp
+++ Device/dvc_referee.cpp
@@ -2,10 +2,10 @@
 #include "cmsis_os.h"
 #include "cmsis_os2.h"
 #include "projdefs.h"
-#include "ui_interface.h"
 #include <cstdint>
 #include <cstring>
 #include "crc.h"
+#include "bsp_usart.h"
 void Referee::Init()
 {
     static const osThreadAttr_t kRefereeTaskAttr = {
@@ -58,26 +58,26 @@ void Referee::RxCpltCallback(uint8_t *buffer, uint16_t length)
         {
             case kStatusDataId:
             {
-                UBaseType_t uxSavedInterruptStatus = taskENTER_CRITICAL_FROM_ISR();
+                // UBaseType_t uxSavedInterruptStatus = taskENTER_CRITICAL_FROM_ISR();
                 std::memcpy(&status_, tmp_buffer->data, sizeof(StatusData));
                 
                 has_received_rx_msg_ = true;
 
-                taskEXIT_CRITICAL_FROM_ISR(uxSavedInterruptStatus);
+                // taskEXIT_CRITICAL_FROM_ISR(uxSavedInterruptStatus);
                 break;
             }
             case kShootDataId:
             {
-                UBaseType_t uxSavedInterruptStatus = taskENTER_CRITICAL_FROM_ISR();
+                // UBaseType_t uxSavedInterruptStatus = taskENTER_CRITICAL_FROM_ISR();
                 std::memcpy(&shoot_, tmp_buffer->data, sizeof(ShootData));
-                taskEXIT_CRITICAL_FROM_ISR(uxSavedInterruptStatus);
+                // taskEXIT_CRITICAL_FROM_ISR(uxSavedInterruptStatus);
                 break;
             }
             case kGameStatusId:
             {
-                UBaseType_t uxSavedInterruptStatus = taskENTER_CRITICAL_FROM_ISR();
+                // UBaseType_t uxSavedInterruptStatus = taskENTER_CRITICAL_FROM_ISR();
                 std::memcpy(&game_status_, tmp_buffer->data, sizeof(GameStatus));
-                taskEXIT_CRITICAL_FROM_ISR(uxSavedInterruptStatus);
+                // taskEXIT_CRITICAL_FROM_ISR(uxSavedInterruptStatus);
                 break;
             }
             default:
@@ -88,46 +88,271 @@ void Referee::RxCpltCallback(uint8_t *buffer, uint16_t length)
     }
 }
 
+void print_message(const uint8_t *message, const int length) {
+
+    // 等待 DMA 空闲
+    while (!g_uart1_manage_object.tx_cplt_flag);
+    memcpy(g_uart1_manage_object.tx_buffer, message, length);
+    g_uart1_manage_object.tx_cplt_flag = false;
+    HAL_UART_Transmit_DMA(
+        g_uart1_manage_object.uart_handler, 
+        g_uart1_manage_object.tx_buffer, 
+        length
+    );
+}
+#define DEFINE_FRAME_PROC(num, id)                          \
+void Referee::ui_proc_ ## num##_frame(ui_ ## num##_frame_t *msg) {   \
+    msg->header.SOF = 0xA5;                                 \
+    msg->header.length = 6 + 15 * num;                      \
+    msg->header.seq = seq++;                                \
+    msg->header.crc8 = verify_crc8_check_sum((uint8_t*)msg, 4);        \
+    msg->header.cmd_id = 0x0301;                            \
+    msg->header.sub_id = id;                                \
+    msg->header.send_id = ui_self_id;                       \
+    msg->header.recv_id = ui_self_id + 256;                 \
+    msg->crc16 = verify_crc16_check_sum((uint8_t*)msg, 13 + 15 * num); \
+}
+
+DEFINE_FRAME_PROC(1, 0x0101)
+DEFINE_FRAME_PROC(2, 0x0102)
+DEFINE_FRAME_PROC(5, 0x0103)
+DEFINE_FRAME_PROC(7, 0x0104)
+
+void Referee::ui_proc_string_frame(ui_string_frame_t *msg) {
+    msg->header.SOF = 0xA5;
+    msg->header.length = 51;
+    msg->header.seq = seq++;
+    msg->header.crc8 = verify_crc8_check_sum((uint8_t *) msg, 4);
+    msg->header.cmd_id = 0x0301;
+    msg->header.sub_id = 0x0110;
+    msg->header.send_id = ui_self_id;
+    msg->header.recv_id = ui_self_id + 256;
+    msg->option.str_length = strlen(msg->option.string);
+    msg->crc16 = verify_crc16_check_sum((uint8_t *) msg, 58);
+}
+
+void Referee::ui_init_0() 
+{
+    for (int i = 0; i < 2; i++) {
+        ui_0_.data[i].figure_name[0] = 0;
+        ui_0_.data[i].figure_name[1] = 0;
+        ui_0_.data[i].figure_name[2] = i + 0;
+        ui_0_.data[i].operate_type = 1;
+    }
+    // for (int i = 2; i < 2; i++) {
+    //     ui_0_.data[i].operate_type = 0;
+    // }
+
+    ui_chassis_l_->figure_type = 0;
+    ui_chassis_l_->operate_type = 1;
+    ui_chassis_l_->layer = 0;
+    ui_chassis_l_->color = 4;
+    ui_chassis_l_->start_x = 0;
+    ui_chassis_l_->start_y = 499;
+    ui_chassis_l_->width = 3;
+    ui_chassis_l_->end_x = 820;
+    ui_chassis_l_->end_y = 646;
+
+    ui_chassis_r_->figure_type = 0;
+    ui_chassis_r_->operate_type = 1;
+    ui_chassis_r_->layer = 0;
+    ui_chassis_r_->color = 4;
+    ui_chassis_r_->start_x = 1916;
+    ui_chassis_r_->start_y = 500;
+    ui_chassis_r_->width = 3;
+    ui_chassis_r_->end_x = 1100;
+    ui_chassis_r_->end_y = 645;
+
+    ui_proc_2_frame(&ui_0_);
+    SEND_MESSAGE((uint8_t *) &ui_0_, sizeof(ui_0_));
+}
+
+void Referee::ui_update_0() 
+{
+    for (int i = 0; i < 2; i++) {
+        ui_0_.data[i].operate_type = 2;
+    }
+
+    ui_proc_2_frame(&ui_0_);
+    SEND_MESSAGE((uint8_t *) &ui_0_, sizeof(ui_0_));
+}
+
+void Referee::ui_remove_0() 
+{
+    for (int i = 0; i < 2; i++) {
+        ui_0_.data[i].operate_type = 3;
+    }
+
+    ui_proc_2_frame(&ui_0_);
+    SEND_MESSAGE((uint8_t *) &ui_0_, sizeof(ui_0_));
+}
+
+void Referee::ui_init_1()
+{
+    ui_1_.option.figure_name[0] = 0;
+    ui_1_.option.figure_name[1] = 0;
+    ui_1_.option.figure_name[2] = 2;
+    ui_1_.option.operate_type = 1;
+
+    ui_spin_->figure_type = 7;
+    ui_spin_->operate_type = 1;
+    ui_spin_->layer = 0;
+    ui_spin_->color = 5;
+    ui_spin_->start_x = 110;
+    ui_spin_->start_y = 760;
+    ui_spin_->width = 2;
+    ui_spin_->font_size = 20;
+    ui_spin_->str_length = 4;
+    strcpy(ui_spin_->string, "SPIN");
+
+    ui_proc_string_frame(&ui_1_);
+    SEND_MESSAGE((uint8_t *) &ui_1_, sizeof(ui_1_));
+
+}
+
+void Referee::ui_update_1()
+{
+    ui_1_.option.operate_type = 2;
+
+    ui_spin_->color = spin_status_ ? 2 : 5;
+
+    ui_proc_string_frame(&ui_1_);
+    SEND_MESSAGE((uint8_t *) &ui_1_, sizeof(ui_1_));
+
+}
+
+void Referee::ui_remove_1()
+{
+    ui_1_.option.operate_type = 3;
+
+    ui_proc_string_frame(&ui_1_);
+    SEND_MESSAGE((uint8_t *) &ui_1_, sizeof(ui_1_));
+
+}
+
+void Referee::ui_init_2()
+{
+    ui_2_.option.figure_name[0] = 0;
+    ui_2_.option.figure_name[1] = 0;
+    ui_2_.option.figure_name[2] = 3;
+    ui_2_.option.operate_type = 1;
+
+    ui_booster_->figure_type = 7;
+    ui_booster_->operate_type = 1;
+    ui_booster_->layer = 0;
+    ui_booster_->color = 5;
+    ui_booster_->start_x = 110;
+    ui_booster_->start_y = 720;
+    ui_booster_->width = 2;
+    ui_booster_->font_size = 20;
+    ui_booster_->str_length = 7;
+    strcpy(ui_booster_->string, "BOOSTER");
+
+    ui_proc_string_frame(&ui_2_);
+    SEND_MESSAGE((uint8_t *) &ui_2_, sizeof(ui_2_));
+}
+
+void Referee::ui_update_2()
+{
+    ui_2_.option.operate_type = 2;
+
+    ui_booster_->color = booster_status_ ? 2 : 5;
+
+    ui_proc_string_frame(&ui_2_);
+    SEND_MESSAGE((uint8_t *) &ui_2_, sizeof(ui_2_));
+
+}
+
+void Referee::ui_remove_2()
+{
+    ui_2_.option.operate_type = 3;
+
+    ui_proc_string_frame(&ui_2_);
+    SEND_MESSAGE((uint8_t *) &ui_2_, sizeof(ui_2_));
+
+}
+
+void Referee::ui_init_3()
+{
+    ui_3_.option.figure_name[0] = 0;
+    ui_3_.option.figure_name[1] = 0;
+    ui_3_.option.figure_name[2] = 6;
+    ui_3_.option.operate_type = 1;
+
+    ui_supercap_->figure_type = 7;
+    ui_supercap_->operate_type = 1;
+    ui_supercap_->layer = 0;
+    ui_supercap_->color = 5;
+    ui_supercap_->start_x = 110;
+    ui_supercap_->start_y = 800;
+    ui_supercap_->width = 2;
+    ui_supercap_->font_size = 20;
+    ui_supercap_->str_length = 8;
+    strcpy(ui_supercap_->string, "SUPERCAP");
+
+    ui_proc_string_frame(&ui_3_);
+    SEND_MESSAGE((uint8_t *) &ui_3_, sizeof(ui_3_));
+}
+
+void Referee::ui_update_3()
+{
+    ui_3_.option.operate_type = 2;
+
+    ui_supercap_->color = supercap_status_ ? 2 : 5;
+
+    ui_proc_string_frame(&ui_3_);
+    SEND_MESSAGE((uint8_t *) &ui_3_, sizeof(ui_3_));
+
+}
+
+void Referee::ui_remove_3()
+{
+    ui_3_.option.operate_type = 3;
+
+    ui_proc_string_frame(&ui_3_);
+    SEND_MESSAGE((uint8_t *) &ui_3_, sizeof(ui_3_));
+}
+
+void Referee::InitUI()
+{
+    ui_init_0();
+    ui_init_1();
+    ui_init_2();
+    ui_init_3();
+}
+
+void Referee::UpdateUI()
+{
+    ui_update_0();
+    ui_update_1();
+    ui_update_2();
+    ui_update_3();
+}
+
+void Referee::RemoveUI()
+{
+    ui_remove_0();
+    ui_remove_1();
+    ui_remove_2();
+    ui_remove_3();
+}
+
 void Referee::Task()
 {
     for (;;)
     {
-        FreshDynamicUI();
-
+        ui_self_id = status_.id;
+        
         if (!ui_inited_ && has_received_rx_msg_)
         {
-            ui_init_booster_off();
+            InitUI();
             ui_inited_ = true;
         }
 
         if (ui_inited_)
         {
-            ui_update_booster_off();
+            UpdateUI();
         }
-        osDelay(pdMS_TO_TICKS(500));
+        osDelay(pdMS_TO_TICKS(100));
     }
-}
-
-void Referee::FreshDynamicUI()
-{
-    // 从裁判系统状态包同步本机器人 ID
-    if (status_.id != 0) {
-        ui_self_id = status_.id;
-        // ui_self_id = 3; // 目前先写死为1，后续根据实际情况修改
-    }
-
-    // booster 状态文字：booster_on / booster_off
-    const char* booster_str = booster_status_ ? "booster_on" : "booster_off";
-    strcpy(ui_booster_off_dynamic_group_booster_off_text->string, booster_str);
-    ui_booster_off_dynamic_group_booster_off_text->str_length = strlen(booster_str);
-    ui_booster_off_dynamic_group_booster_off_text->color = booster_status_ ? 2 : 5;
-
-    // 小陀螺状态文字：spin_on / spin_off
-    const char* spin_str = spin_status_ ? "spin_on" : "spin_off";
-    strcpy(ui_booster_off_dynamic__group_spin_off_text->string, spin_str);
-    ui_booster_off_dynamic__group_spin_off_text->str_length = strlen(spin_str);
-    ui_booster_off_dynamic__group_spin_off_text->color = spin_status_ ? 2 : 5;
-
-    // 超级电容状态文字颜色
-    ui_booster_off_dynamic__group_cap_charge_text->color = supercap_status_ ? 2 : 5;
 }
--- Device/dvc_referee.h
+++ Device/dvc_referee.h
@@ -1,8 +1,96 @@
 #pragma  once
 #include "stdlib.h"
 #include "stdint.h"
-#include "ui.h"
 #include <cstdint>
+
+#define PRIMITIVE_CAT(x, y) x ## y
+#define CAT(x, y) PRIMITIVE_CAT(x, y)
+
+#define DEFINE_MESSAGE(name, p_a, p_b, p_c, p_d, p_e)   \
+struct ui_interface_ ## name ##_t{       \
+uint8_t figure_name[3];                                 \
+uint32_t operate_type:3;                                \
+uint32_t figure_type:3;                                 \
+uint32_t layer:4;                                       \
+uint32_t color:4;                                       \
+uint32_t PRIMITIVE_CAT(,p_a) :9;                        \
+uint32_t PRIMITIVE_CAT(,p_b):9;                         \
+uint32_t width:10;                                      \
+uint32_t start_x:11;                                    \
+uint32_t start_y:11;                                    \
+uint32_t PRIMITIVE_CAT(,p_c):10;                        \
+uint32_t PRIMITIVE_CAT(,p_d):11;                        \
+uint32_t PRIMITIVE_CAT(,p_e):11;                        \
+}__attribute__((packed));                                                      
+
+DEFINE_MESSAGE(figure, _a, _b, _c, _d, _e);
+DEFINE_MESSAGE(line, _a, _b, _c, end_x, end_y);
+DEFINE_MESSAGE(rect, _a, _b, _c, end_x, end_y);
+DEFINE_MESSAGE(round, _a, _b, r, _d, _e);
+DEFINE_MESSAGE(ellipse, _a, _b, _c, rx, ry);
+DEFINE_MESSAGE(arc, start_angle, end_angle, _c, rx, ry);
+
+struct ui_interface_number_t{
+    uint8_t figure_name[3];
+    uint32_t operate_type: 3;
+    uint32_t figure_type: 3;
+    uint32_t layer: 4;
+    uint32_t color: 4;
+    uint32_t font_size: 9;
+    uint32_t _b: 9;
+    uint32_t width: 10;
+    uint32_t start_x: 11;
+    uint32_t start_y: 11;
+    int32_t number;
+}__attribute__((packed));
+
+struct ui_interface_string_t{
+    uint8_t figure_name[3];
+    uint32_t operate_type: 3;
+    uint32_t figure_type: 3;
+    uint32_t layer: 4;
+    uint32_t color: 4;
+    uint32_t font_size: 9;
+    uint32_t str_length: 9;
+    uint32_t width: 10; 
+    uint32_t start_x: 11;
+    uint32_t start_y: 11;
+    uint32_t _c: 10;
+    uint32_t _d: 11;
+    uint32_t _e: 11;
+    char string[30];
+}__attribute__((packed));
+
+struct ui_frame_header_t{
+    uint8_t SOF;
+    uint16_t length;
+    uint8_t seq, crc8;
+    uint16_t cmd_id, sub_id;
+    uint16_t send_id, recv_id;
+} __attribute__((packed));
+
+#define DEFINE_FIGURE_MESSAGE(num)              \
+struct ui_ ## num##_frame_t {    \
+ui_frame_header_t header;                       \
+ui_interface_figure_t data[num];                \
+uint16_t crc16;                                 \
+}__attribute__((packed));
+
+DEFINE_FIGURE_MESSAGE(1);
+DEFINE_FIGURE_MESSAGE(2);
+DEFINE_FIGURE_MESSAGE(5);
+DEFINE_FIGURE_MESSAGE(7);
+
+struct ui_string_frame_t{
+    ui_frame_header_t header;
+    ui_interface_string_t option;
+    uint16_t crc16;
+}__attribute__((packed));
+
+
+void print_message(const uint8_t* message, int length);
+
+#define SEND_MESSAGE(message, length) print_message(message, length)
 
 /**
  * @brief 裁判系统命令码类型
@@ -104,6 +192,9 @@ class Referee{
 public:
     void Init();
     void Task();
+    void InitUI();
+    void UpdateUI();
+    void RemoveUI();
     void FreshDynamicUI();
     void FreshStaticUI();
     void RxCpltCallback(uint8_t *buffer, uint16_t length);
@@ -126,8 +217,46 @@ public:
     StatusData status_{0};
     ShootData shoot_{};
     GameStatus game_status_{};
+
+    int ui_self_id;
+    uint8_t seq = 0;
 private:
     static void TaskEntry(void *param);  // FreeRTOS 入口，静态函数
+
+    char chassis_flag[8]; 
+
+    ui_2_frame_t ui_0_;
+    ui_string_frame_t ui_1_;
+    ui_string_frame_t ui_2_;
+    ui_string_frame_t ui_3_;
+
+    ui_interface_line_t *ui_chassis_l_ = (ui_interface_line_t*)&(ui_0_.data[0]);
+    ui_interface_line_t *ui_chassis_r_ =(ui_interface_line_t*)&(ui_0_.data[1]);
+    ui_interface_string_t *ui_spin_ = &(ui_1_.option);
+    ui_interface_string_t *ui_booster_ = &(ui_2_.option);
+    ui_interface_string_t *ui_supercap_ = &(ui_3_.option);
+
+    void ui_proc_1_frame(ui_1_frame_t *msg);
+    void ui_proc_2_frame(ui_2_frame_t *msg);
+    void ui_proc_5_frame(ui_5_frame_t *msg);
+    void ui_proc_7_frame(ui_7_frame_t *msg);
+    void ui_proc_string_frame(ui_string_frame_t *msg);
+    
+    void ui_init_0();
+    void ui_update_0();
+    void ui_remove_0();
+
+    void ui_init_1();
+    void ui_update_1();
+    void ui_remove_1();
+
+    void ui_init_2();
+    void ui_update_2();
+    void ui_remove_2();
+
+    void ui_init_3();
+    void ui_update_3();
+    void ui_remove_3();
 
     volatile bool has_received_rx_msg_ = false;
     bool ui_inited_ = false;
